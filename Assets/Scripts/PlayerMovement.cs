dd200d53ddaad3afc4da8e4b7c5ee7079e6570dd
unknown <lawalleo390@gmail.com>
2026-04-21 10:51:37 +0200

    V0.07 - Dash + Cooldown

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,29 +1,57 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Rigidbody2D body;
+    private Rigidbody2D body;
 
-    float horizontal;
-    float vertical;
-    float moveLimiter = 0.7f;
-
+    private float horizontal;
+    private float vertical;
+    private float moveLimiter = 0.7f;
+    private Vector3 moveDirection;
+    private float _timer;
+    private bool _canDash;
+    
+    
+    public float dashDistance = 1.2f;
     public float runSpeed = 20.0f;
+    public float dashCoolDown = 1f;
+    
 
-    void Start ()
+    void Awake ()
     {
         body = GetComponent<Rigidbody2D>();
+        
     }
 
     void Update()
     {
+        //  ---- Mouvement et Direction ----
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+        moveDirection = new Vector3(horizontal, vertical).normalized;
+
+        //  ---- Timer Cooldown ----
+        _timer += Time.deltaTime;
+        Debug.Log(_timer);
+        if (_timer <= dashCoolDown)
+        {
+            _canDash = false;
+        }
+        else
+        {
+            _canDash = true;
+        }
+        
+        //  ---- Dash ----
+        
         Dash();
+        
     }
 
     void FixedUpdate()
     {
+        
         if (horizontal != 0 && vertical != 0)
         {
             horizontal *= moveLimiter;
@@ -31,10 +59,21 @@ public class PlayerMovement : MonoBehaviour
         } 
 
         body.linearVelocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+        
     }
 
     private void Dash()
     {
+        if (_canDash)
+        {
+           if (Input.GetKeyDown(KeyCode.Space))
+           {
+               body.MovePosition(transform.position + moveDirection * dashDistance);
+               _timer = 0f;
+           } 
+        }
         
+            
+            
     }
 }
