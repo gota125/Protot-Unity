74284f9ff80b2fc1569c746f932a4a1de407b537
unknown <lawalleo390@gmail.com>
2026-04-22 10:20:28 +0200

    V0.20 - Map update + Wall

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,8 +1,10 @@
+using System;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
     public Vector3 speed;
+    
 
     void Start()
     {
@@ -19,5 +21,14 @@ public class Projectile : MonoBehaviour
     public void Movement()
     {
         transform.position = transform.position + speed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+        
     }
 }
