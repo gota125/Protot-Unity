c1756f6813a63b6d842d37ccb672ac4d4f921fbf
gota125 <36821665+gota125@users.noreply.github.com>
2026-04-23 09:19:40 +0200

    V0.28

---
+++ Assets/Scripts/invokeEnemy.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class invokeEnemy : MonoBehaviour
+{
+   [SerializeField] private GameObject Enemy;
+
+   private void Start()
+   {
+      Instantiate(Enemy, new Vector3(0,0,0), transform.rotation);
+   }
+}
